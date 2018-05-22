14b87052265fd61d2ee20b1354fd6073d989b17c
= <trent@mabank.tech>
2018-05-22 00:46:42 +0000

    update admin.py

--- login/admin.py
+++ login/admin.py
@@ -12,23 +12,13 @@ admin.site.register(Users)
 class characterAdmin(admin.ModelAdmin):
 
     list_per_page = 1500
-    #actions = [balance, markAsCheck, markAsOnline]
-    #inlines = [
-    #    songInline,
-    #]
-    #fields = ('user_id','name','albumReleaseDate','premiumRequired','albumImage')
-    list_display = ('user_id','name','class_field','gender','health','level','experience','clan')
+   list_display = ('user_id','name','class_field','gender','health','level','experience','clan')
 admin.site.register(Characters, characterAdmin)
    
     
 class activeLoginsAdmin(admin.ModelAdmin):
 
     list_per_page = 1500
-    #actions = [balance, markAsCheck, markAsOnline]
-    #inlines = [
-    #    songInline,
-    #]
-    #fields = ('user_id','name','albumReleaseDate','premiumRequired','albumImage')
     list_display = ('user_id','session_key','character_id')
     readonly_fields = ('character_id','user_id')
 admin.site.register(ActiveLogins, activeLoginsAdmin)
